c39079e3daa7cdb585e796d4f78d5ce759c610ef
eMobilis <fathiabdikadir09@gmail.com>
2023-02-09 09:15:00 +0300

    ifstatements

---
+++ conditions/ifstatement.py
@@ -0,0 +1,14 @@
+dial = int(input("Enter a number"))
+if dial == 1:
+    print("data deals")
+elif dial == 2:
+    print("daily bundles")
+elif dial == 3:
+    print("GO MONTHLY")
+elif dial  == 4:
+    print("weekly bundles")
+elif dial == 5:
+    print("no expiry")
+else:
+    print("more")
+
---
+++ datastructures/dictionary.py
@@ -0,0 +1,6 @@
+#store data in key and value pairs
+#use curly brackets
+friends = {"loui": 25, "lisa": 30, "jake": 50}
+friends["felix"] = 22
+print(friends)
+

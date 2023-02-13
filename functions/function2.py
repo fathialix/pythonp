bc4703057b001c29a54e1724557428a28f9703dc
eMobilis <fathiabdikadir09@gmail.com>
2023-02-13 09:09:32 +0300

    functiontwo

---
+++ functions/function2.py
@@ -0,0 +1,6 @@
+def calctwo():
+    num1 = int(input("enter first number"))
+    num2 = int(input("enter second number"))
+    area = num1 * num2
+    print(f"area of the object is {area}")
+calctwo()

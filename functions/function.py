58cade612618303c08e2d47b03fe8617bc25be12
eMobilis <fathiabdikadir09@gmail.com>
2023-02-13 08:54:42 +0300

    functionsexe

---
+++ functions/function.py
@@ -0,0 +1,7 @@
+def shutdown():
+    print("do you want to shut down the machine")
+shutdown()
+
+def restartmachine():
+    print("restart machine")
+restartmachine()
---
+++ functions/functionexe.py
@@ -0,0 +1,5 @@
+def calc_months():
+    age = int(input("Enter your age"))
+    months_lived = age * 12
+    print(f"you have lived for {months_lived} months")
+calc_months()
